fe0e286c16579098019e2e74db4704fa39d9a3be
Marcos Neves <marcos.neves@gmail.com>
2013-05-19 12:27:29 -0300

    separate google analytics

--- php/tpl/layout/_scripts.php
+++ php/tpl/layout/_scripts.php
@@ -4,9 +4,4 @@
 <script src="/assets/js/plugins.js"></script>
 <script src="/assets/js/app.js"></script>
 
-<script> // Google Analytics
-    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-        g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-        s.parentNode.insertBefore(g,s)}(document,'script'));
-</script>
+<?php include '_ga.php';
