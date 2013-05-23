45d6fd504ae45d09c5a22c5dad870356e1060dbf
Marcos Neves <marcos.neves@gmail.com>
2013-05-23 11:55:49 -0300

    php/bin/cleanup.sh

---
+++ php/bin/cleanup.sh
@@ -0,0 +1,10 @@
+DIR=$(cd `dirname $0` && pwd)
+cd $DIR/../../
+
+echo Remove git inside php vendors
+find ./php/vendor -name ".git" | wc -l
+find ./php/vendor -name ".git" -exec rm -rf {} \;
+
+echo Remove .DS_Store
+find . -name ".DS_Store" | wc -l
+find . -name ".DS_Store" -exec rm -rf {} \;
