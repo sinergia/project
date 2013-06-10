f2f4c6688655c8c0a99c87927008d453e743c2f7
Marcos Neves <marcos.neves@gmail.com>
2013-06-10 12:00:24 -0300

    setup error configuration

--- php/config.php
+++ php/config.php
@@ -7,6 +7,13 @@
 // utiliza o autoload do composer, para auto carregar as classes
 require_once 'vendor/autoload.php';
 
+error_reporting(E_ALL);
+
+\Sinergia\Error::toException();
+\Sinergia\Error::enableDefaultExceptionHandler();
+
+\Analog\Analog::handler( \Analog\Handler\ChromeLogger::init() );
+
 set_include_path(__DIR__.'/tpl');
 
 $app = new App\App();
