418a910c4e7ec76d93a9d74efe7a2c7ba16c3c91
Marcos Neves <marcos.neves@gmail.com>
2013-06-10 11:25:58 -0300

    update assets/vendor

--- php/tpl/layout/_vendors-js.php
+++ php/tpl/layout/_vendors-js.php
@@ -1,3 +1,2 @@
-<script src="/assets/vendor/jquery/2.0.0/jquery.min.js"></script>
-<script src="/assets/vendor/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
-<script src="/assets/vendor/jquery-fullscreen-plugin/1.1.4/jquery.fullscreen-min.js"></script>
+<script src="/assets/vendor/jquery/2.0.1/jquery.min.js"></script>
+<script src="/assets/vendor/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
