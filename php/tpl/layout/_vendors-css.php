1ced19a2e963990b1f1ce0eae6fdd6271b908051
Marcos Neves <marcos.neves@gmail.com>
2013-06-24 00:00:29 -0300

    upgrade jquery + font awesome

--- php/tpl/layout/_vendors-css.php
+++ php/tpl/layout/_vendors-css.php
@@ -1,3 +1,3 @@
 <link rel="stylesheet" href="/assets/vendor/twitter-bootstrap/2.3.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="/assets/vendor/twitter-bootstrap/2.3.2/css/bootstrap-responsive.min.css">
-<link rel="stylesheet" href="/assets/vendor/font-awesome/3.1.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/assets/vendor/font-awesome/3.2.0/css/font-awesome.min.css">
--- php/tpl/layout/_vendors-js.php
+++ php/tpl/layout/_vendors-js.php
@@ -1,2 +1,2 @@
-<script src="/assets/vendor/jquery/2.0.1/jquery.min.js"></script>
+<script src="/assets/vendor/jquery/2.0.2/jquery.min.js"></script>
 <script src="/assets/vendor/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
