4bd07f6237460995385e391512bdd4ef8db4541d
Marcos Neves <marcos.neves@gmail.com>
2013-08-09 15:07:24 -0300

    now REQUEST_TIME_FLOAT is done by sinergia/php-forward-compat

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 
-// compativel com php 5.4
-isset($_SERVER['REQUEST_TIME_FLOAT'])
-    or $_SERVER['REQUEST_TIME_FLOAT'] = microtime(true);
-
 // versão mínima do php necessária
 version_compare(PHP_VERSION, '5.3.7', '>=')
     or exit('precisa do PHP 5.3.7 ou superior. Versao atual: ' . PHP_VERSION);
