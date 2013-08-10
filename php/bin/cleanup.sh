7ae98df2146e2354ef2755c37bae3d206b0edd9c
Marcos Neves <marcos.neves@gmail.com>
2013-08-10 11:48:12 -0300

    cleanup .orig files

--- php/bin/cleanup.sh
+++ php/bin/cleanup.sh
@@ -8,3 +8,7 @@ find ./php/vendor -name ".git" -exec rm -rf {} \;
 echo Remove .DS_Store
 find . -name ".DS_Store" | wc -l
 find . -name ".DS_Store" -exec rm -rf {} \;
+
+echo Remove .orig files from merge
+find . -name "*.orig" | wc -l
+find . -name "*.orig" -exec rm {} \;
