8de94fe45d4665370d33690c807e27935f320d03
Marcos Neves <marcos.neves@gmail.com>
2013-11-14 15:23:52 -0200

    update vendors

--- php/tpl/layout/_vendors-css.php
+++ php/tpl/layout/_vendors-css.php
@@ -1,2 +1,2 @@
-<link rel="stylesheet" href="/assets/vendor/twitter-bootstrap/3.0.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="/assets/vendor/font-awesome/3.2.1/css/font-awesome.min.css">
+<link rel="stylesheet" href="/assets/vendor/twitter-bootstrap/3.0.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="/assets/vendor/font-awesome/4.0.1/css/font-awesome.min.css">
